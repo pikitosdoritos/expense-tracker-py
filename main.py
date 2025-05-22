c0fb9fef501b27ecee4c3856b21e1c37df071a9c
pikitosdoritos <fastovetsn@gmail.com>
2025-05-22 16:51:15 +0300

    Translate in English

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
 def main():
-    print("Простой калькулятор расходов")
+    print("Simple cost calculator")
     expenses = []
 
     while True:
-        entry = input("Введите сумму расхода (или 'стоп' для завершения): ")
-        if entry.lower() == 'стоп':
+        entry = input("(Write here the amount of expenses) or to stop write 'stop'")
+        if entry.lower() == 'stop':
             break
+
         try:
             amount = float(entry)
             expenses.append(amount)
-        except ValueError:
-            print("Пожалуйста, введите число!")
 
-    if expenses:
-        total = sum(expenses)
-        average = total / len(expenses)
-        print(f"\nИтоговая сумма расходов: {total:.2f} руб.")
-        print(f"Средний расход: {average:.2f} руб.")
-    else:
-        print("Нет введённых расходов.")
+        except ValueError:
+            print("WRITE A NUMBER!")
+
+        if expenses:
+            total = sum(expenses)
+            average = total/len(expenses)
+            print(f"\nTotal {total: .2f} $")
+            print(f"\nAverage {average: .2f} $")
+
+        else:
+            print("NO COST")
 
 if __name__ == "__main__":
     main()
-
---
+++ project.py
@@ -0,0 +1,72 @@
+#1
+# l = [] 
+# lis = [1, 56, 'x', 2.34, ['s', 't', 'r', 'o', 'k', 'a']]
+# print (lis)
+
+# a = [a + b for a in 'list' if a != 's' for b in 'soup' if b != 'u']
+# print (a)
+
+# lox = [a + b for a in 'pidor' if a != 'o' for b in 'gay' if b!= 'g']
+# print(lox)
+
+# l.append(23)
+# l.append(23)
+# l.append(24)
+# b = [24, 57]
+# l.extend (b)
+# l.insert (1, 56)
+# l.remove (24)
+# l.pop (1)
+# print (l.index(57))
+# print (l.count(34))
+# l.sort()
+# l.reverse()
+# l.clear()
+
+#2
+# print (l)
+
+# item[START:STOP:STEP]
+# l = [34, 'sd', 56, 34.34]
+# print('\n' + str(l[-1]))
+
+# i = 0
+# while i < 4: 
+#     print(l[i])
+#     i += 1
+
+# print (l[::-2])
+
+#3
+# a = (43, 56, 45.23, 'd') #кортеж
+# b = [43, 56, 45.23, 'd']
+
+# print (a.__sizeof__())
+# print (b.__sizeof__())
+
+# a = tuple("hi")
+# print ("\n" + str(a))
+
+# a = "hello word", "sdf", 345
+# print (a)
+
+# print(a.count(345))
+
+#4
+# d = {'test' : 1, 'test_2' : "TeSt"} #словарь
+# print(d['test_2'])
+
+# d = dict(short='dict', longer='dictionary')
+# d['short'] = 2345
+# print(d)
+
+# d = dict ([(23, 'gg'), (56, 87)])
+# print(d)
+
+# d = dict.fromkeys (['a', 'b'], 1)
+
+# print (d)
+
+# d = {a: a**2 for a in range(7)}
+# print (d)
+
